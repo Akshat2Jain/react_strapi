013f6bacefbeb5e74eb2f3e7cf35dd56007333b2
akshat jain <akshat.2024it1031@kiet.edu>
2022-09-09 16:53:32 +0530

    strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '592db4961e4ccb9cc01e87ab64809485'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6a1262f0-c5dd-4131-9e02-84b4cf5e8ff7'
+};
